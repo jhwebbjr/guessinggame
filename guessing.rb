8166cdd83e236f5da598e1eb68037cd25d793517
James Webb <webbfamily@Jamess-MBP.lan>
2017-04-18 15:13:54 -0400

    Added repeated number functionality

--- guessing.rb
+++ guessing.rb
@@ -1,9 +1,8 @@
+require 'pry'
 lotto = rand(100)
 
 puts lotto
 play = "Pick a number from 1 to 100."
-guess = gets.chomp.to_i
-# puts guess
 
 def lower(guess, lotto)
   guess < lotto
@@ -17,20 +16,31 @@ def winner(guess, lotto)
   guess == lotto
 end
 
+stored_guesses = []
+
+def repeated_guess(stored_guesses, guess)
+  stored_guesses.include?(guess)
+end
+
 count = 0
 5.times do
   puts play
   guess = gets.chomp.to_i
-  if lower(guess, lotto)
+  if repeated_guess(stored_guesses, guess)
+    puts "Please do not repeat the same number twice."
+    count +=1
+    stored_guesses << guess
+  elsif lower(guess, lotto)
     puts "Too low."
     count +=1
+    stored_guesses << guess
   elsif higher(guess, lotto)
     puts "Too high."
     count +=1
+    stored_guesses << guess
   elsif winner(guess, lotto)
     puts "You win!"
     break
-  else
-    puts "You have exhausted your game plays."
   end
 end
+puts "You've exhausted your game plays. You lose."
